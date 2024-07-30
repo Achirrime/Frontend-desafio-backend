afd3cb16feddf602dd6cdfa6395d83ab5f66284c
Achirrime <alcindachirrime@gmail.com>
2024-07-30 16:11:27 +0200

    add autoplay

--- src/components/slieder.jsx
+++ src/components/slieder.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -6,20 +6,20 @@ import Img1 from '../assets/img1.jpg';
 import Img2 from '../assets/img2.jpg';
 import Img3 from '../assets/img3.jpg';
 import Img4 from '../assets/im4.jpg';
-import Img6 from '../assets/img5.jpg';
 import Img5 from '../assets/menu.jpg';
+import Img6 from '../assets/img5.jpg';
 import Img7 from '../assets/img7.jpg';
 import Img8 from '../assets/img8.jpg';
 
 const imagens = [
-  { image: Img1},
-  { image: Img2},
-  { image: Img3},
-  { image: Img4},
-  { image: Img5},
-  { image: Img7},
-  { image: Img8},
-  { image: Img6},
+  { image: Img1 },
+  { image: Img2 },
+  { image: Img3 },
+  { image: Img4 },
+  { image: Img5 },
+  { image: Img6 },
+  { image: Img7 },
+  { image: Img8 },
 ];
 
 export const Featured = () => {
@@ -31,6 +31,8 @@ export const Featured = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,            
+    autoplaySpeed: 2000,        
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     responsive: [
@@ -52,26 +54,19 @@ export const Featured = () => {
   };
 
   return (
-    
-     
-
-      <div className="py-4 max-w-[1200px] m-auto">
-        <Slider {...settings}>
-          {(showAll ? imagens : imagens.slice(0, 8)).map((imagem, index) => (
-            <div key={index} className="px-4">
-              <img
-                className="w-full h-60 object-cover rounded-2xl"
-                src={imagem.image}
-                alt={imagem.name}
-              />
-              <p className="text-center mt-2">{imagem.name}</p>
-              <p className="text-center pb-2 font-bold">
-                {imagem.price}
-              </p>
-            </div>
-          ))}
-        </Slider>
-      </div>
+    <div className="py-4 max-w-[1200px] m-auto">
+      <Slider {...settings}>
+        {(showAll ? imagens : imagens.slice(0, 8)).map((imagem, index) => (
+          <div key={index} className="px-4">
+            <img
+              className="w-full h-60 object-cover rounded-2xl"
+              src={imagem.image}
+              alt={`Imagem ${index + 1}`}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
